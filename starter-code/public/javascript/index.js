8b261bfc8e828327a30385c9c5b0eb7b2c197e6f
Alejandro Salas <asalas.wd@outlook.com>
2019-08-19 21:22:52 -0400

    add all characters info to DOM after clicking on fetch all

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,6 +2,27 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
+    axios.get('https://ih-crud-api.herokuapp.com/characters')
+    .then((result) => {
+      let allChar = result.data;
+      let allCharDiv = $('.characters-container')
+
+      for(let i = 0; i< allChar.length; i++){        
+       
+        allCharDiv.append(
+        `<div class="character-info">
+          <div class="name">${allChar[i].name}</div>
+          <div class="occupation">${allChar[i].occupation}</div>
+          <div class="cartoon">${allChar[i].debt}</div>
+          <div class="weapon">${allChar[i].weapon}</div>
+        </div>
+        `
+
+        )
+      }
+
+      
+    });        
 
   }
   
